88d35b878103f3fdbc8145b0665d8d95665ae8b5
TigranKh21 <tigran.khachaturyants@gmail.com>
2023-10-07 21:15:52 +0300

    task-2

--- src/js/task-02.js
+++ src/js/task-02.js
@@ -6,3 +6,17 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+
+const ingrListEl = document.querySelector('#ingredients');
+
+const ingredientList = [];
+
+ingredients.forEach((ingredient) => {
+  const listItem = document.createElement('li');
+  listItem.textContent = ingredient;
+  listItem.classList.add('item');
+  ingredientList.push(listItem)
+});
+
+
+ingrListEl.append(...ingredientList);
