99946841f8a563f2b399e68e6ef58f1a1e0268cb
TigranKh21 <tigran.khachaturyants@gmail.com>
2023-10-08 20:12:02 +0300

    task-9 currentColor is added

--- src/js/task-09.js
+++ src/js/task-09.js
@@ -3,3 +3,16 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+console.log(getRandomHexColor());
+const body = document.querySelector('body');
+const changeColorBtn = document.querySelector('.change-color');
+const currentColor = document.querySelector('.color');
+
+changeColorBtn.addEventListener('click', onChangeColor);
+
+function onChangeColor() {
+  const newColor = getRandomHexColor();
+  body.style.backgroundColor = `${newColor}`;
+  currentColor.textContent = `${newColor}`;
+}
